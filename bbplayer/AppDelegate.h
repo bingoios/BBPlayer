2f8c0a80439c6239b7da0913f45b8b02b619d813
bingoli <bingoli86@qq.com>
2015-02-20 22:47:07 +0800

    添加工程文件的内容

---
+++ bbplayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bbplayer
+//
+//  Created by lib－mac on 15-2-20.
+//  Copyright (c) 2015年 everydaygame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
